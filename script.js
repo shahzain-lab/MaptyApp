1d99335507c8f0c6466defea068a099971379d0b
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-08-18 14:47:45 +0500

    styling leaflet features

--- script.js
+++ script.js
@@ -11,28 +11,63 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+//Global VAR
+let map;
+let mapEvent;
+
 //Geolocation API
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-    const coords = [latitude, longitude];
+      const coords = [latitude, longitude];
 
-    const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-      .openPopup();
-  },
-  function () {
-    alert("Could not get permission");
-  }
-);
+      map.on("click", function (mapEve) {
+        mapEvent = mapEve;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert("Could not get permission");
+    }
+  );
+
+form.addEventListener("submit", function (e) {
+  //prevent default
+  e.preventDefault();
+
+  //clear value
+  inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+    inputCadence.value =
+      "";
+
+  //display workout
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        className: "running-popup",
+        autoClose: false,
+        closeOnClick: false,
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
